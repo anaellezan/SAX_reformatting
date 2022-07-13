53d36224fc4f79d4537b95a8a5b9382363c36e9a
Marta Núñez García <marnugar@gmail.com>
2022-07-13 15:44:35 +0200

    Update workaround_visualization_issue.py

--- workaround_visualization_issue.py
+++ workaround_visualization_issue.py
@@ -3,23 +3,13 @@
 
 import SimpleITK as sitk
 
-# im_filename = '/home/marta/Desktop/DATA/Hugo/weird_case/image_0-sax.mha'
-# mask_filename = '/home/marta/Desktop/DATA/Hugo/weird_case/image_0_lvendo-sax.mha'
-
-
-# im_filename = '/home/marta/Desktop/DATA/Sonny/ICD_cancel_TA/wrong-sax/0101597655/ct1-sax-iso.mha'
-# mask_filename = '/home/marta/Desktop/DATA/Sonny/ICD_cancel_TA/wrong-sax/0101597655/ct1-lvendo-sax-iso.mha'
-
-#im_filename = '/home/marta/Desktop/DATA/Sonny/ICD_cancel_TA/wrong-sax/0103193704/ct1-sax-iso.mha'
-#mask_filename = '/home/marta/Desktop/DATA/Sonny/ICD_cancel_TA/wrong-sax/0103193704/ct1-lvendo-sax-iso.mha'
-
-im_filename = '/home/marta/Desktop/DATA/Sonny/ICD_cancel_TA/wrong-sax/0126577926/ct1-sax-iso.mha'
-mask_filename = '/home/marta/Desktop/DATA/Sonny/ICD_cancel_TA/wrong-sax/0126577926/ct1-lvendo-sax-iso.mha'
+im_filename = 'example_pat0/ct-sax.mha'
+mask_filename = 'example_pat0/ct-lvendo-sax.mha'
 
 im_sax = sitk.ReadImage(im_filename)
 mask_sax = sitk.ReadImage(mask_filename)
 
-print('Computed direction (not easy to display)', im_sax.GetDirection())
+#print('Computed direction (not easy to display)', im_sax.GetDirection())
 new_direction = [1, 0, 0,
                  0, 0, -1,
                  0, 1, 0]
